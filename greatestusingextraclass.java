e1c136cd546b40869e9d665bda15f4eb44240de7
Himanshu Jaiswal <55348957+itshimanshu010@users.noreply.github.com>
2023-05-01 10:19:34 +0530

    Create greatestusingextraclass.java

---
+++ greatestusingextraclass.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+class Greatest 
+{
+	
+	int num1,num2,num3;
+	
+	void getdata()
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter first number= ");
+		num1=input.nextInt();
+		System.out.println("Enter second number= ");
+		num2=input.nextInt();
+		System.out.println("Enter third number= ");
+		num3=input.nextInt();
+		if(num1>=num2 && num1>=num3)
+		{
+			System.out.println("Greatest is "+num1);
+		}
+		
+		else if(num2>=num1 && num2>=num3)
+		{
+			System.out.println("Greatest is "+num2);
+		}
+		
+		else if(num3>=num1 && num3>=num2)
+		{
+			System.out.println("Greatest is "+num3);
+		}
+		
+		else
+		{
+			System.out.println("All are Equal");
+		}
+	}
+}
+
+
+public class greatestusingextraclass 
+{
+
+	public static void main(String[] args) 
+    {
+		     Greatest g=new Greatest();
+		     g.getdata();
+    }
+  
+}
